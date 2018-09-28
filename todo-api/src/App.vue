843979c743367d5fd0d3b3da6962082f3c83ebd7
beckychien <becky6110@gmail.com>
2018-09-28 11:01:04 +0800

    first commit

---
+++ todo-api/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- 使用ToDo組件 -->
+    <ToDo msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+//導入組件
+//import HelloWorld from './components/HelloWorld.vue'
+import ToDo from './components/ToDo.vue'
+
+
+export default {
+  name: 'app',
+  //註冊組件
+  components: { 
+    ToDo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ todo-api/src/components/ToDo.vue
@@ -0,0 +1,62 @@
+<template>
+	<div class="view">
+		<h1>{{title}}</h1>
+		<input v-model="newTodo" @keyup.enter="addData" 
+		placeholder="Anything todo?" />
+		<ul>
+			<!-- finish這個class透過itme中isFinished屬性決定是否新增 -->
+			<li v-for="item in items" 
+			v-bind:class="{finish:item.isFinished}"
+			@click="toggleFinish(item)">
+			{{item.text}}
+		</li>
+	</ul>
+</div>
+</template>
+<script>
+	export default{
+		name:'ToDo',
+		data(){
+			return{
+				title:"TodoList",
+				items:[],
+				newTodo:''
+			}
+		},
+		methods:{
+			toggleFinish:function(item){
+				item.isFinished=!item.isFinished;
+			},
+			addData:function(){
+				this.items.push(
+				{
+					text:this.newTodo,
+					isFinished:false	
+				},
+				);
+				this.newTodo="";
+			}
+		}
+	}
+
+
+</script>
+
+<style>
+.view{
+	width:400px;
+	border:1pz solid gray;
+	margin:20px auto;
+}
+.view .title{
+
+}
+.view ul li{
+	margin:10px 0;
+	list-style:none;
+}
+.view .finish{
+	color:gray;
+	text-decoration:line-through;
+}
+</style>
